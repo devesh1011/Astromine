139488555008b02017a9c0b724a768095cb474b2
devesh1011 <116812354+devesh1011@users.noreply.github.com>
2025-03-20 15:50:32 +0530

    Update createPost and main files to introduce a new asteroid-themed post type and modify the post creation process, including changes to labels and titles for better alignment with the new theme.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import './createPost.js';
 
 import { Devvit, useState, useWebView } from '@devvit/public-api';
-
 import type { DevvitMessage, WebViewMessage } from './message.js';
 
 Devvit.configure({
@@ -9,91 +8,28 @@ Devvit.configure({
   redis: true,
 });
 
-// Add a custom post type to Devvit
 Devvit.addCustomPostType({
-  name: 'Web View Example',
-  height: 'tall',
+  name: "Astromine",
+  description: "An asteroid mining game.",
   render: (context) => {
-    // Load username with `useAsync` hook
-    const [username] = useState(async () => {
-      return (await context.reddit.getCurrentUsername()) ?? 'anon';
-    });
+    const webview = useWebView<WebViewMessage, DevvitMessage>({
+      url: "page.html",
+      async onMessage(message, webview) {
+        message.type = "webViewReady"
+      }
+    })
 
-    // Load latest counter from redis with `useAsync` hook
-    const [counter, setCounter] = useState(async () => {
-      const redisCount = await context.redis.get(`counter_${context.postId}`);
-      return Number(redisCount ?? 0);
-    });
-
-    const webView = useWebView<WebViewMessage, DevvitMessage>({
-      // URL of your web view content
-      url: 'page.html',
-
-      // Handle messages sent from the web view
-      async onMessage(message, webView) {
-        switch (message.type) {
-          case 'webViewReady':
-            webView.postMessage({
-              type: 'initialData',
-              data: {
-                username: username,
-                currentCounter: counter,
-              },
-            });
-            break;
-          case 'setCounter':
-            await context.redis.set(
-              `counter_${context.postId}`,
-              message.data.newCounter.toString()
-            );
-            setCounter(message.data.newCounter);
-
-            webView.postMessage({
-              type: 'updateCounter',
-              data: {
-                currentCounter: message.data.newCounter,
-              },
-            });
-            break;
-          default:
-            throw new Error(`Unknown message type: ${message satisfies never}`);
-        }
-      },
-      onUnmount() {
-        context.ui.showToast('Web view closed!');
-      },
-    });
-
-    // Render the custom post type
     return (
-      <vstack grow padding="small">
-        <vstack grow alignment="middle center">
-          <text size="xlarge" weight="bold">
-            Example App
-          </text>
-          <spacer />
-          <vstack alignment="start middle">
-            <hstack>
-              <text size="medium">Username:</text>
-              <text size="medium" weight="bold">
-                {' '}
-                {username ?? ''}
-              </text>
-            </hstack>
-            <hstack>
-              <text size="medium">Current counter:</text>
-              <text size="medium" weight="bold">
-                {' '}
-                {counter ?? ''}
-              </text>
-            </hstack>
-          </vstack>
-          <spacer />
-          <button onPress={() => webView.mount()}>Launch App</button>
-        </vstack>
-      </vstack>
-    );
-  },
-});
+      <button onPress={() => webview.mount()}>Watch Asteroid</button>
+    )
+  }
+})
+
+// Devvit.addSchedulerJob({
+//   name: "New Asteroid Appeared",
+//   onRun: async (event, context) {
+
+//   }
+// })
 
 export default Devvit;
