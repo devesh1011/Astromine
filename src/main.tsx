61a1e1921509be8b3a93345043e9ab4df420c7c2
yaseen2402 <myaseen2402@gmail.com>
2025-03-21 23:53:27 +0530

    add redis

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import "./createPost.js";
 
-import { Devvit, useState, useWebView } from "@devvit/public-api";
+import { Devvit, useState, useAsync, useWebView } from "@devvit/public-api";
 import type { DevvitMessage, WebViewMessage } from "./message.js";
 
 Devvit.configure({
@@ -12,10 +12,82 @@ Devvit.addCustomPostType({
   name: "Astromine",
   description: "An asteroid mining game.",
   render: (context) => {
+    const postId = context.postId;
+
+    const [username] = useState(async () => {
+      return (await context.reddit.getCurrentUsername()) ?? 'anon';
+    });
+
+    // Load latest counter from redis with `useAsync` hook
+    
+
+    const { data: playerItems, loading: playerItemsLoading } = useAsync(
+      async () => (await context.redis.hGetAll(`items_${username}_${postId}`)) ?? []
+    );
+
+    const { data: playerEquips, loading: playerEquipsLoading } = useAsync(
+      async () => (await context.redis.hGetAll(`equips_${username}_${postId}`)) ?? []
+    );
+
     const webview = useWebView<WebViewMessage, DevvitMessage>({
       url: "page.html",
-      async onMessage(message, webview) {
-        message.type = "webViewReady";
+      async onMessage(message, webView) {
+        switch (message.type) {
+          case 'webViewReady':
+            if(!playerEquipsLoading && !playerItemsLoading){
+              webView.postMessage({
+                type: 'initialData',
+                data: {
+                  username: username,
+                  playerItems: playerItems,
+                  playerEquips: playerEquips
+                },
+              });
+            }
+            break;
+          case 'updateEquips':
+            if(message.data.updateType=="increase"){
+              await context.redis.hIncrBy(
+                `items_${username}_${postId}`, 
+                message.data.equipName, 
+                parseInt(message.data.equipValue)
+              );
+            }
+
+            else if(message.data.updateType=="decrease"){
+              await context.redis.hIncrBy(
+                `items_${username}_${postId}`, 
+                message.data.equipName, 
+                -parseInt(message.data.equipValue)
+              );
+            }
+            console.log(`Updated equip values`);
+            break;
+
+          case 'updateItems':
+            if(message.data.updateType=="increase"){
+              await context.redis.hIncrBy(
+                `items_${username}_${postId}`, 
+                message.data.itemName, 
+                parseInt(message.data.itemValue)
+              );
+            }
+
+            else if(message.data.updateType=="decrease"){
+              await context.redis.hIncrBy(
+                `items_${username}_${postId}`, 
+                message.data.itemName, 
+                -parseInt(message.data.itemValue)
+              );
+            }
+            console.log(`Updated item values`);
+            break;
+          default:
+            throw new Error(`Unknown message type: ${message satisfies never}`);
+        }
+      },
+      onUnmount() {
+        context.ui.showToast('Web view closed!');
       },
     });
 
